a3ebacf90d2abe2ee0926631dc175aa2ffd07955
Andrey Sorokin <sorokin0andrey@gmail.com>
2019-05-13 20:13:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { RNAlerter } = NativeModules;
 
 export default class Alerter {
   static show({ title = '', message = '', backgroundColor = '', duration = 4000 }) {
     RNAlerter.show({
-      title: title.length ? title : message,
-      message: title.length ? message : '',
       backgroundColor,
       duration,
+      ...Platform.select({
+        ios: {
+          title: title || undefined,
+          message: message || undefined,
+        },
+        android: {
+          title,
+          message,
+        },
+      })
     });
   }
 };
